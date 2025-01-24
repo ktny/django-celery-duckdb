cda02d6dcc29f6a065944eeca36add94d2e32e00
ktny <katsunori.t10@gmail.com>
2025-01-24 23:14:45 +0900

    Add Docker Compose for Celery and RabbitMQ services

--- src/hello/views.py
+++ src/hello/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 
+from .tasks import add
+
 
 # Create your views here.
 def index(request: HttpRequest):
-    return HttpResponse("hello world")
+    task_id = add.delay(4, 4)
+    return HttpResponse(task_id)
